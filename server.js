ccf745d2e1fcf6dd7922dd4d86370895122dc3a9
Justine Oyaghiro <justineoyaghiro@icloud.com>
2023-08-17 21:06:57 -0500

    Add book route

--- server.js
+++ server.js
@@ -10,10 +10,13 @@ app.use(cors());
 
 const PORT = process.env.PORT || 3001;
 
+const Book = require("./models/books");
+
 app.get('/test', (request, response) => {
 
   response.send('test request received')
 
 })
+
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
