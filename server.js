884561b1c900249d74b9854cee4e72c097156ae1
Gerard <gerard.g.johnson@gmail.com>
2023-08-20 14:46:54 -0500

    created mongo

--- server.js
+++ server.js
@@ -1,37 +1,31 @@
-'use strict';
+"use strict";
 
-const mongoose = require('mongoose');
-require('dotenv').config();
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-main().catch(err => console.log(err));
+main().catch((err) => console.log(err));
 
 console.log(process.env.DATABASE_URL);
 
 async function main() {
   await mongoose.connect(process.env.DATABASE_URL);
-console.log('Mongoose connected successfully');
+  console.log("Mongoose connected successfully");
 }
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
 app.use(cors());
 
 const PORT = process.env.PORT || 3001;
 
-const Book = require('./models/books');
+const Book = require("./models/books");
 
-app.get('/books', async (request, response, next) => {
-
+app.get("/books", async (request, response, next) => {
   const filterQuery = {};
-
-  
-})
-app.get('/test', (request, response) => {
-
-  response.send('test request received')
-
-})
-
+});
+app.get("/test", (request, response) => {
+  response.send("test request received");
+});
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
