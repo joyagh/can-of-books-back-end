9a9aa851dabba26e892d96ed92c49b00acd9bd65
Bianca Chery <127991428+BiancaChery@users.noreply.github.com>
2023-08-31 01:14:32 +0000

    Added delete and update

--- server.js
+++ server.js
@@ -26,10 +26,29 @@ const bookModel = require('./models/books');
 app.get('/books', async (request, response, next) => {
 let books = await bookModel.find()
 response.status(200).send(books)
-  
+});
 
-  
-})
+app.post('/books', async (request, response) => {
+  let { title, description, status } = request.body;
+  let newBook = await bookModel.create({ title, description, status });
+  response.send(newBook);
+});
+
+app.delete('/books/:id', async (request, response) => {
+  await bookModel.findByIdAndDelete(request.params.id);
+  response.send('Deleted!');
+});
+
+app.put('/books/:id', async (request, response) => {
+  let options = {
+    new: true,
+  }
+  let books_id = books._id;
+  let updatedBook = await bookModel.findByIdAndUpdate(books_id, { Firm Commitments }, options);
+console.log(printBooks(updatedBook));
+}); 
+
+
 app.get('/test', (request, response) => {
 
   response.send('test request received')
